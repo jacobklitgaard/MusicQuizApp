c604d0b2ea89ecff61a636a5e7d825b12ce125db
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2026-04-13 15:15:42 +0200

    first commit

---
+++ src/main/java/org/example/musicquizapp/MusicQuizAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.musicquizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicQuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/musicquizapp/MusicQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.musicquizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
